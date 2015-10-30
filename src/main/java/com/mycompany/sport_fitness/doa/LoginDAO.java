879cee1fda2688dd5421b7ebf03a9d3db606f28c
heito <heito@Heitor>
2015-10-30 20:41:50 -0200

    Atualização2

---
+++ src/main/java/com/mycompany/sport_fitness/doa/LoginDAO.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sport_fitness.doa;
+
+/**
+ *
+ * @author heito
+ */
+import com.mycompany.spot_fitness.entidade.LoginBean;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class LoginDAO extends Conexao {
+
+    private Connection con = null;
+    private ResultSet rs = null;
+    private PreparedStatement pst = null;
+
+    public void cadastrarLogin(LoginBean user) {
+        String sql = "insert into Usuarios(nameUser,pasUser) value(?,?)";
+
+        try {
+            con = conexao();
+            pst = con.prepareStatement(sql);
+            pst.setString(1, user.getNameUser());
+            pst.setString(2, user.getPasUser());
+
+            pst.executeQuery();
+        } catch (SQLException | ClassNotFoundException error) {
+            System.out.println(error);
+        }
+    }
+
+    public boolean selecionarLogin(String nameUser, String pasUser) {
+        String sql = "select from Usuarios where nameUser = ? and pasUser = ?";
+
+        try {
+            con = conexao();
+            pst = con.prepareStatement(sql);
+            pst.setString(1, nameUser);
+            pst.setString(2, pasUser);
+            pst.executeQuery();
+        } catch (SQLException | ClassNotFoundException error) {
+            System.out.println(error);
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/mycompany/spot_fitness/servlet/LoginServlet.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.spot_fitness.servlet;
+
+import com.mycompany.sport_fitness.doa.LoginDAO;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author heito
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String user = request.getParameter("username");
+        JOptionPane.showMessageDialog(null,user);
+        System.out.println(user);
+        String password = request.getParameter("password");
+        JOptionPane.showMessageDialog(null,password);
+        System.out.println(password);
+        
+        try {
+            LoginDAO login = new LoginDAO();
+            login.selecionarLogin(user, password);
+            response.sendRedirect("../html/Home.html");
+        } catch (Exception error) {
+            JOptionPane.showMessageDialog(null,error);
+            System.out.println(error);
+        }
+        
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
