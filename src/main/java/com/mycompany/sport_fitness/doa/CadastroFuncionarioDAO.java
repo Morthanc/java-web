29a6db6b771de6e79faac32d923cc78e41a78fe4
uriel.pgoliveira <uriel.pgoliveira@CAS486190C14727.senacsp.edu.br>
2015-11-03 21:49:54 -0200

    início de programação de cadastro de funcionários - falta implementar SQL

--- src/main/java/com/mycompany/sport_fitness/doa/CadastroFuncionarioDAO.java
+++ src/main/java/com/mycompany/sport_fitness/doa/CadastroFuncionarioDAO.java
@@ -5,10 +5,64 @@
  */
 package com.mycompany.sport_fitness.doa;
 
+import com.mycompany.spot_fitness.entidade.CadastroFuncionarioBean;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
- * @author heito
+ * @author uriel
  */
-public class CadastroFuncionarioDAO {
+public class CadastroFuncionarioDAO extends Conexao{
     
+    private Connection con = null;
+    private ResultSet rs = null;
+    private PreparedStatement pst = null;
+    
+    
+    
+            
+    
+     public void create(CadastroFuncionarioBean user) {
+        String sql = "INSERT INTO Usuarios(x,x) value(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+        try {
+            con = conexao();
+            pst = con.prepareStatement(sql);
+            
+            //dados pessoais
+            pst.setString(1, user.getNomeFuncionario());
+            pst.setObject(2, user.getDataNascimento());
+            pst.setObject(3, user.getSexo());
+            
+            //documentos
+            pst.setString(4, user.getRg());
+            pst.setString(5, user.getOrgaoEmissor());
+            pst.setString(6, user.getLocalEmissao());
+            pst.setString(7, user.getCpf());
+            
+            
+            //naturalização
+            pst.setString(8, user.getPaisOrigem());
+            pst.setString(9, user.getCidade());
+            pst.setString(10, user.getEstado());
+            
+            //endereço
+            pst.setString(11, user.getRua());
+            pst.setString(12, user.getNumero());
+            pst.setString(13, user.getBairro());
+            pst.setString(14, user.getCep());
+            pst.setString(15, user.getComplemento());
+            pst.setString(16, user.getTelefone());
+            pst.setString(17, user.getCelular());
+            
+
+            pst.executeQuery();
+            con.close();
+        } catch (SQLException | ClassNotFoundException error) {
+            System.out.println(error);
+        }
+    }
 }
--- src/main/java/com/mycompany/spot_fitness/entidade/CadastroFuncionarioBean.java
+++ src/main/java/com/mycompany/spot_fitness/entidade/CadastroFuncionarioBean.java
@@ -5,10 +5,181 @@
  */
 package com.mycompany.spot_fitness.entidade;
 
+import java.sql.Date;
+
 /**
  *
  * @author heito
  */
-public class CadastroFuncionarioBean {
+public class CadastroFuncionarioBean { 
+     
+     //dados pessoais
+   
+    private String nomeFuncionario;   
+    private Date dataNascimento; 
+    private Object sexo;
+    
+    public String getNomeFuncionario() {
+        return nomeFuncionario;
+    }
+
+    public void setNomeFuncionario(String nomeFuncionario) {
+        this.nomeFuncionario = nomeFuncionario;
+    }
+    
+     public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
+    public Object getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Object sexo) {
+        this.sexo = sexo;
+    }
+    
+    
+    //documentos de identificação
+    private String rg;    
+    private String orgaoEmissor;
+    private String localEmissao;
+    private String cpf;
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getOrgaoEmissor() {
+        return orgaoEmissor;
+    }
+
+    public void setOrgaoEmissor(String orgaoEmissor) {
+        this.orgaoEmissor = orgaoEmissor;
+    }
+
+    public String getLocalEmissao() {
+        return localEmissao;
+    }
+
+    public void setLocalEmissao(String localEmissao) {
+        this.localEmissao = localEmissao;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+          
+    
+    
+    
+    
+    //naturalização
+    private String paisOrigem;
+    private String cidade;
+    private String estado;
+    
+    public String getPaisOrigem() {
+        return paisOrigem;
+    }
+
+    public void setPaisOrigem(String paisOrigem) {
+        this.paisOrigem = paisOrigem;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+    //endereço
+    private String rua;
+    private String bairro;
+    private String numero;
+    private String cep;
+    private String complemento;
+    private String telefone;
+    private String celular;
+     
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+    
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
     
 }
