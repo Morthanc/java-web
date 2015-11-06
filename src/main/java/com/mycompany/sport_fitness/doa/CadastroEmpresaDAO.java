794d7451711e3c5816c15739448831fb33a184fd
ASUS <ASUS@192.168.36.108>
2015-11-06 17:16:30 -0200

    CRUD Empresa incompleto - servlet fudido além da compreensão humana

--- src/main/java/com/mycompany/sport_fitness/doa/CadastroEmpresaDAO.java
+++ src/main/java/com/mycompany/sport_fitness/doa/CadastroEmpresaDAO.java
@@ -5,10 +5,159 @@
  */
 package com.mycompany.sport_fitness.doa;
 
+import com.mycompany.spot_fitness.entidade.CadastroEmpresaBean;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
- * @author heito
+ * @author uriel
  */
-public class CadastroEmpresaDAO {
+public class CadastroEmpresaDAO extends Conexao{
+      
+    private Connection con = null;
+    private ResultSet rs = null;
+    private PreparedStatement pst = null;
+
+     public void create(CadastroEmpresaBean empresa) {
+        String sql = "INSERT INTO Funcionarios(cnpj, cei, razaoSocial, nomeFantasia, gerenteResponsavel, "
+                + "logradouro, numero, complemento, bairro, cep, cidade, estado, telefone, "
+                + "celular, email) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+        try {
+            con = conexao();
+            pst = con.prepareStatement(sql);
+            
+            
+            pst.setString(1, empresa.getCnpj());
+            pst.setString(2, empresa.getCei());
+            pst.setString(3, empresa.getRazaoSocial());
+            pst.setString(4, empresa.getNomeFantasia());
+            pst.setString(5, empresa.getGerenteResponsavel());
+            pst.setString(6, empresa.getLogradouro());
+               pst.setInt(7, empresa.getNumero());
+            pst.setString(8,  empresa.getComplemento());
+            pst.setString(9,  empresa.getBairro());
+            pst.setString(10, empresa.getCep());
+            pst.setString(11, empresa.getCidade());
+            pst.setString(12, empresa.getEstado());
+            pst.setString(13, empresa.getTelefone());
+            pst.setString(14, empresa.getCelular());
+            pst.setString(15, empresa.getEmail());
+            
+
+            rs = pst.executeQuery();
+            con.close();
+        } catch (SQLException | ClassNotFoundException error) {
+            System.out.println(error);
+        }
+    }
+     
+     public void read(CadastroEmpresaBean empresa){
+     String sql = "INSERT INTO Funcionarios(cnpj, cei, razaoSocial, nomeFantasia, gerenteResponsavel, "
+                + "logradouro, numero, complemento, bairro, cep, cidade, estado, telefone, "
+                + "celular, email) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+     
+      try {
+            con = conexao();
+            pst = con.prepareStatement(sql);
+            
+            
+            pst.setString(1, empresa.getCnpj());
+            pst.setString(2, empresa.getCei());
+            pst.setString(3, empresa.getRazaoSocial());
+            pst.setString(4, empresa.getNomeFantasia());
+            pst.setString(5, empresa.getGerenteResponsavel());
+            pst.setString(6, empresa.getLogradouro());
+               pst.setInt(7, empresa.getNumero());
+            pst.setString(8,  empresa.getComplemento());
+            pst.setString(9,  empresa.getBairro());
+            pst.setString(10, empresa.getCep());
+            pst.setString(11, empresa.getCidade());
+            pst.setString(12, empresa.getEstado());
+            pst.setString(13, empresa.getTelefone());
+            pst.setString(14, empresa.getCelular());
+            pst.setString(15, empresa.getEmail());
+            
+
+            rs = pst.executeQuery();
+            con.close();
+        } catch (SQLException | ClassNotFoundException error) {
+            System.out.println(error);
+        }
+     }
+     
+     
+     public void update(CadastroEmpresaBean empresa){
+     String sql = "UPDATE TABLE Funcionarios(cnpj, cei, razaoSocial, nomeFantasia, gerenteResponsavel, "
+                + "logradouro, numero, complemento, bairro, cep, cidade, estado, telefone, "
+                + "celular, email) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+      try {
+            con = conexao();
+            pst = con.prepareStatement(sql);
+            
+            
+            pst.setString(1, empresa.getCnpj());
+            pst.setString(2, empresa.getCei());
+            pst.setString(3, empresa.getRazaoSocial());
+            pst.setString(4, empresa.getNomeFantasia());
+            pst.setString(5, empresa.getGerenteResponsavel());
+            pst.setString(6, empresa.getLogradouro());
+               pst.setInt(7, empresa.getNumero());
+            pst.setString(8,  empresa.getComplemento());
+            pst.setString(9,  empresa.getBairro());
+            pst.setString(10, empresa.getCep());
+            pst.setString(11, empresa.getCidade());
+            pst.setString(12, empresa.getEstado());
+            pst.setString(13, empresa.getTelefone());
+            pst.setString(14, empresa.getCelular());
+            pst.setString(15, empresa.getEmail());
+            
+
+            rs = pst.executeQuery();
+            con.close();
+        } catch (SQLException | ClassNotFoundException error) {
+            System.out.println(error);
+        }
+     }
+     
+     
+     public void delete(CadastroEmpresaBean empresa){
+     String sql = "INSERT INTO Funcionarios(cnpj, cei, razaoSocial, nomeFantasia, gerenteResponsavel, "
+                + "logradouro, numero, complemento, bairro, cep, cidade, estado, telefone, "
+                + "celular, email) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+     
+      try {
+            con = conexao();
+            pst = con.prepareStatement(sql);
+            
+            
+            pst.setString(1, empresa.getCnpj());
+            pst.setString(2, empresa.getCei());
+            pst.setString(3, empresa.getRazaoSocial());
+            pst.setString(4, empresa.getNomeFantasia());
+            pst.setString(5, empresa.getGerenteResponsavel());
+            pst.setString(6, empresa.getLogradouro());
+               pst.setInt(7, empresa.getNumero());
+            pst.setString(8,  empresa.getComplemento());
+            pst.setString(9,  empresa.getBairro());
+            pst.setString(10, empresa.getCep());
+            pst.setString(11, empresa.getCidade());
+            pst.setString(12, empresa.getEstado());
+            pst.setString(13, empresa.getTelefone());
+            pst.setString(14, empresa.getCelular());
+            pst.setString(15, empresa.getEmail());
+            
+
+            rs = pst.executeQuery();
+            con.close();
+        } catch (SQLException | ClassNotFoundException error) {
+            System.out.println(error);
+        }
+     }
+     
+     
     
 }
